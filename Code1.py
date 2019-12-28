89dc24081a1ba86c74bb0a58ea89ac89badb6362
kdohyeong <do5568@naver.com>
2019-12-28 17:44:00 +0900

    [String indexing and slicing]

--- Code1.py
+++ Code1.py
@@ -1,3 +1,24 @@
 print("=" * 50)
 print("My Program")
 print("=" * 50)
+
+multiline='''
+Life is too short
+You need python
+'''
+
+a = "Life is too short, You need Python"
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[-1])
+print(a[-5])
+
+b = a[0] + a[1] + a[2] + a[3] + a[4]
+
+c = a[0:10]
+print(b)
+print(c)
+
