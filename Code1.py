d7729d1f56277a4be4561266c15043099210a7cf
kdohyeong <do5568@naver.com>
2019-12-28 18:17:42 +0900

    [Formatting Function]

--- Code1.py
+++ Code1.py
@@ -1,11 +1,14 @@
+'''
+
+
 print("=" * 50)
 print("My Program")
 print("=" * 50)
 
-multiline='''
+multiline=''
 Life is too short
 You need python
-'''
+''
 
 a = "Life is too short, You need Python"
 
@@ -39,3 +42,26 @@ print("I ate %d apples. so I was sick for %s days." %(number , day))
 print("%10s" % 'Hi')
 print("%-10s" % 'Hi')
 print("%0.4f" %3.248592)
+
+
+'''
+
+
+
+print("I eat {0} apples.".format(3))
+print("I eat {0} melons.".format("Four"))
+number = 7
+print("I eat {0} orange.".format(number))
+day = 10
+print("I ate {0} apples. so I was sick for {1} days.".format(number, day))
+
+print("{0:<15}".format("Hi"))
+print("{0:>15}".format("Hello"))
+print("{0:^15}".format("Python"))
+print("{0:=>15}".format("Destiny"))
+print("{0:=^15}".format("KDHyeong"))
+
+name = 'Kimdohyeong'
+age = 26
+
+print(f'My name is {name} i am {age} years old')
